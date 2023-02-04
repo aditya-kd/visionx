37b2d481c8447d83bb0ea410bc57d500e8a9fd46
Ayushhgupta39 <ayushgupta3902@gmail.com>
2023-02-05 01:42:52 +0530

    #2 Responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,149 @@
-import { Box, Button, ListItem, Text, UnorderedList } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ListItem,
+  Text,
+  UnorderedList,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Flex,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
   return (
-    <Box display={"flex"} justifyContent={"space-between"}>
-      <UnorderedList listStyleType={"none"} display={"flex"} className="navbar-ul">
-        <ListItem p={'2'} className="navbar-li">
-          <Link to={"/"}>
-          <Text as={"b"} fontSize={'x-large'}>Vision-X</Text>
+    <Box>
+      <Box
+        justifyContent={"space-between"}
+        className="navbar-lg"
+        display={["none", "none", "flex", "flex"]}
+      >
+        <UnorderedList
+          listStyleType={"none"}
+          display={"flex"}
+          className="navbar-ul"
+        >
+          <ListItem p={"2"} className="navbar-li">
+            <Link to={"/"}>
+              <Text as={"b"} fontSize={"x-large"}>
+                Vision-X
+              </Text>
+            </Link>
+          </ListItem>
+        </UnorderedList>
+        <UnorderedList
+          listStyleType={"none"}
+          display={"flex"}
+          className="navbar-ul"
+        >
+          <Link to={"/start-ups"}>
+            <li className="navbar-li">Start-ups</li>
           </Link>
-        </ListItem>
-      </UnorderedList>
-      <UnorderedList listStyleType={"none"} display={"flex"} className="navbar-ul">
-        <Link to={"/start-ups"}>
-        <li className="navbar-li" p={'2'}>Start-ups</li>
-        </Link>
-        <Link to={"/partners"}>
-        <li className="navbar-li">Partners</li>
-        </Link>
-        <Link to={"/investors"}>
-        <li className="navbar-li">Investors</li>
-        </Link>
-        <Link to={"/blog"}>
-        <li className="navbar-li">Blog</li>
-        </Link>
-        <Link to={"/about"}>
-        <li className="navbar-li">About</li>
-        </Link>
-        <ListItem p={'2'}>
-            <Button colorScheme={'messenger'}>Contact Us</Button>
-        </ListItem>
-      </UnorderedList>
+          <Link to={"/partners"}>
+            <li className="navbar-li">Partners</li>
+          </Link>
+          <Link to={"/investors"}>
+            <li className="navbar-li">Investors</li>
+          </Link>
+          <Link to={"/blog"}>
+            <li className="navbar-li">Blog</li>
+          </Link>
+          <Link to={"/about"}>
+            <li className="navbar-li">About</li>
+          </Link>
+          <ListItem p={"3"}>
+            <Button
+              bgColor={"#FFBF23"}
+              color={"white"}
+              size={"sm"}
+              variant={"outline"}
+              outlineColor={"black"}
+              _hover={{ bgColor: "#FFD369" }}
+            >
+              Contact Us
+            </Button>
+          </ListItem>
+        </UnorderedList>
+      </Box>
+
+      <Box
+        className="nav-drawer"
+        display={["flex", "flex", "none", "none"]}
+        justifyContent={"space-between"}
+      >
+        <Flex m={"1"}>
+          <Link to={"/"}>
+            <Text as={"b"} fontSize={"x-large"} className={"navbar-ul"}>
+              Vision-X
+            </Text>
+          </Link>
+        </Flex>
+        <Flex m={"1"}>
+          <Button>
+            <HamburgerIcon ref={btnRef} onClick={onOpen} />
+          </Button>
+  
+          <Drawer
+            isOpen={isOpen}
+            placement="left"
+            onClose={onClose}
+            finalFocusRef={btnRef}
+          >
+            <DrawerOverlay />
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerHeader>Welcome!</DrawerHeader>
+
+              <DrawerBody>
+                <UnorderedList
+                  listStyleType={"none"}
+                  className="navbar-ul"
+                  textAlign={"center"}
+                  onClick={onClose}
+                >
+                  <Link to={"/start-ups"}>
+                    <li className="navbar-li">Start-ups</li>
+                  </Link>
+                  <Link to={"/partners"}>
+                    <li className="navbar-li">Partners</li>
+                  </Link>
+                  <Link to={"/investors"}>
+                    <li className="navbar-li">Investors</li>
+                  </Link>
+                  <Link to={"/blog"}>
+                    <li className="navbar-li">Blog</li>
+                  </Link>
+                  <Link to={"/about"}>
+                    <li className="navbar-li">About</li>
+                  </Link>
+                  <ListItem p={"2"}>
+                    <Button
+                      bgColor={"#FFBF23"}
+                      color={"white"}
+                      size={"sm"}
+                      variant={"outline"}
+                      outlineColor={"black"}
+                      _hover={{ bgColor: "#FFD369" }}
+                    >
+                      Contact Us
+                    </Button>
+                  </ListItem>
+                </UnorderedList>
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
+        </Flex>
+      </Box>
     </Box>
   );
 }
