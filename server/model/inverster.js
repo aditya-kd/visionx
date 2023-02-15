aabe98355a925e236cc0a87cfac5e0476cace29e
Aarav238 <aarav8090shukla@gmail.com>
2023-02-15 09:51:58 +0530

    schema

---
+++ server/model/inverster.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+
+const investorSchema = new mongoose.Schema({
+
+})
+
+
+
+const Investor = mongoose.model("Investors", investorSchema)
---
+++ server/model/startup.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+
+const startupSchema = new mongoose.Schema({
+
+})
+
+
+const Startups = mongoose.model("Startups", startupSchema)
